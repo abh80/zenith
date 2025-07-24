2bdfce2b9d67f707da8e928fa96e564942679299
abh80 <abhb787@outlook.com>
2025-07-24 12:27:16 +0530

    feat: add foldWithRecovery method to SimpleAstVisitor for enhanced error handling

--- src/main/scala/analysis/SimpleAstVisitor.scala
+++ src/main/scala/analysis/SimpleAstVisitor.scala
@@ -1,5 +1,21 @@
 package analysis
 
 import ast.{AstNode, AstVisitor}
+import util.{Problem, Result}
 
-class SimpleAstVisitor(protected val nodes: List[AstNode[?]]) extends AstVisitor
+import scala.collection.mutable
+
+class SimpleAstVisitor(protected val nodes: List[AstNode[?]]) extends AstVisitor {
+  def foldWithRecovery[A](initial: A)(f: (A, AstNode[?]) => Result[A]): Result[A] =
+    val problems = mutable.ListBuffer[Problem]()
+    val result = nodes.foldLeft(initial) { (acc, node) =>
+      f(acc, node) match {
+        case Left(nodeProblems) =>
+          problems ++= nodeProblems
+          acc
+        case Right(newacc) => newacc
+      }
+    }
+    if problems.isEmpty then Right(result) else Left(problems.toList)
+
+}
